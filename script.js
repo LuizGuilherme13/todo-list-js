5d7193ab5bb0870ff3b379da2cfb7b168af3e821
LuizGuilherme13 <luizguilherme.bs13@hotmail.com>
2024-03-10 00:51:10 -0300

    feat: Done and Delete funcs

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const todoList = document.getElementById('todos')
-    todoList.innerHTML = ''
-
-    const todos = getAllTodos()
-
-    for (const todo of todos) {
-        addTodo(todo.todo, todo.key)
-    }
+    refreshTodos()
 })
 
 document.getElementById('btn-add-todo').addEventListener('click', () => {
     let input = document.querySelector('input')
+    const key = `todo-${localStorage.length + 1}`
 
-    let key = localStorage.length + 1
-    localStorage.setItem(key, input.value)
+    const todo = JSON.stringify({ text: input.value, done: false })
+    localStorage.setItem(key, todo)
 
-    addTodo(input.value, key)
+    addTodo(key, input.value)
 
     input.value = ''
 })
@@ -24,18 +18,22 @@ const getAllTodos = () => {
     let todos = []
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i)
-        const todo = localStorage.getItem(key)
+        const value = JSON.parse(localStorage.getItem(key))
 
-        todos.push({ todo: todo, key: key })
+        todos.push({ key: key, text: value.text, done: value.done })
     }
 
     return todos
 }
 
-const addTodo = (text, key) => {
+const addTodo = (key, text, done) => {
     let li = document.createElement('li')
 
     li.classList.add('todo')
+
+    if (done) {
+        li.classList.add('done')
+    }
 
     li.innerHTML = `
         <i class="fa-solid fa-circle-check" style="color: green;"></i>
@@ -43,20 +41,38 @@ const addTodo = (text, key) => {
         <i class="fa-solid fa-trash-can" style="color: red;"></i>
     `
 
-    const todoId = `todo-${key}`
-    li.setAttribute('id', todoId)
+    li.setAttribute('id', key)
 
     const todos = document.getElementById('todos')
     todos.appendChild(li)
 
-    const doneElement = document.querySelector(`#${todoId} .fa-circle-check`)
-    doneElement.addEventListener('click', () => { alert('opa') })
+    const doneElement = document.querySelector(`#${key} .fa-circle-check`)
+    doneElement.addEventListener('click', () => { DoneTodo(key) })
 
-    const deleteElement = document.querySelector(`#${todoId} .fa-trash-can`)
-    deleteElement.addEventListener('click', (e) => { DeleteTodo(e) })
+    const deleteElement = document.querySelector(`#${key} .fa-trash-can`)
+    deleteElement.addEventListener('click', () => { DeleteTodo(key) })
 }
 
-const DoneTodo = () => { }
+const refreshTodos = () => {
+    const todoList = document.getElementById('todos')
+    todoList.innerHTML = ''
 
-const DeleteTodo = (event) => {
+    const todos = getAllTodos()
+
+    for (const todo of todos) {
+        addTodo(todo.key, todo.text, todo.done)
+    }
+}
+
+const DoneTodo = (key) => {
+    let todo = JSON.parse(localStorage.getItem(key))
+    todo.done = !todo.done
+    localStorage.setItem(key, JSON.stringify(todo))
+
+    refreshTodos()
+}
+
+const DeleteTodo = (key) => {
+    document.getElementById(key).remove()
+    localStorage.removeItem(key)
 }
